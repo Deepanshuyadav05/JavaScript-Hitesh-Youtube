0a2fb0c78ddf7dc659f8be015db956da3fd86181
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-10-09 22:15:53 +0530

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let name="deep";
+let age=18;
+let alive=true;
+let state;
+let money=null;
+console.log([typeof name,typeof age, typeof alive, typeof state, typeof money]);
+console.log(typeof null);                  //the null is stand-alone value and its type is object  //null is object
+console.log(typeof undefined);             //the type of undefined is undefined //undefined is itself a type
+
+
+// number => range 2 to power 53
+// bigint => used with big no.
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
